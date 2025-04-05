7508926c6e327647386ce1cca920087a05bc14a4
Hancock <chrisjonhancock@yahoo.com>
2025-04-05 07:39:16 -0500

    Clean out demo app and ignore idea

---
+++ src/main/java/com/process/autopay/AutopayApplication.java
@@ -0,0 +1,13 @@
+package com.process.autopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopayApplication.class, args);
+	}
+
+}
